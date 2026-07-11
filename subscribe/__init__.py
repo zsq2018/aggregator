04eba7f6ed9137fdf3cb4206ce9d24123afe6aa5
zsq2018 <31987547+zsq2018@users.noreply.github.com>
2026-07-11 19:26:10 +0800

    Add core modules: origin, logger, init

---
+++ subscribe/__init__.py
@@ -0,0 +1,6 @@
+"""
+Aggregator - Subscribe
+automatically crawl and aggregate proxies
+"""
+
+__version__ = "1.0.0"
---
+++ subscribe/logger.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+# @Author  : wzdnzd
+# @Time    : 2022-07-15
+
+import logging
+import sys
+
+
+def get_logger(filename: str = "", level: int = logging.INFO) -> logging.Logger:
+    logger = logging.getLogger("aggregator")
+    if logger.handlers:
+        return logger
+
+    logger.setLevel(level=level)
+    format = logging.Formatter("%(asctime)s %(filename)s [line:%(lineno)d] %(levelname)s: %(message)s")
+
+    stream_handler = logging.StreamHandler(stream=sys.stdout)
+    stream_handler.setFormatter(format)
+    stream_handler.setLevel(level=level)
+    logger.addHandler(stream_handler)
+
+    if filename:
+        file_handler = logging.FileHandler(filename=filename, encoding="utf-8", mode="a")
+        file_handler.setFormatter(format)
+        file_handler.setLevel(level=level)
+        logger.addHandler(file_handler)
+
+    return logger
+
+
+default_logger = get_logger()
